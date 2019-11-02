8de956a93391927139bd5d04e640cbd17e446869
Tridib Samanta <45572985+tridibsamanta@users.noreply.github.com>
2019-11-02 20:34:06 +0530

    Add files via upload

---
+++ CPP005_Arithmetic_Assignment_Increment_Decrement_Operators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+    int a=10,b=5;
+
+    cout << a + b <<endl; //Addition
+    cout << a - b <<endl; //Subtraction
+    cout << a / b <<endl; //Division
+    cout << a * b <<endl; //Multiplication
+
+    cout << a % b <<endl; //Modulo -> Remainder
+
+    cout << ++a <<endl; //Pre-Increment
+    cout << --a <<endl; //Pre-decrement
+    cout << a++ <<endl; //Post-Increment
+    cout << a-- <<endl; //Post-decrement
+
+}
