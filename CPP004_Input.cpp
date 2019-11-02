ab8a09fe44c26acff1417c25f7f15226b55af526
Tridib Samanta <45572985+tridibsamanta@users.noreply.github.com>
2019-11-02 20:18:14 +0530

    Add files via upload

---
+++ CPP004_Input.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+    // cin - console input
+    int a;
+    cin >> a;
+    cout << "Variable name a = " << a << endl;
+
+    string name,surname;
+
+    cout << "Enter your name : ";
+    cin >> name;
+
+    cout << "Enter your surname : ";
+    cin >> surname;
+
+    cout << "Welcome "<<name<<" "<<surname<<endl;
+
+}
