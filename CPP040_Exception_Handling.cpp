633fcf0b729006b60c9f17ac9068425a7d4789a5
Hussein Sawan <96922361+hu-sawan@users.noreply.github.com>
2023-08-17 16:34:48 +0300

    Update CPP040_Exception_Handling.cpp
    Edited the thrown exception so it is more specified

--- CPP040_Exception_Handling.cpp
+++ CPP040_Exception_Handling.cpp
@@ -1,41 +1,39 @@
-/**
- * Author:    Tridib Samanta
- * Created:   03.03.2020
- **/
+#include <iostream>
+#include <exception>
+#include <stdexcept>
 
- #include<iostream>
- using namespace std;
+using namespace std;
 
- class DivisionByZeroException
- {
-    public:
-        void getErrorMessage()
-        {
-            cout<<"Error : Do not divide by zero !"<<endl;
-        }
- };
+class DivisionByZeroException : public invalid_argument
+{
+public:
+    explicit DivisionByZeroException(const string &msg) : invalid_argument(msg) {}
+};
 
- double division(double a,double b) throw(DivisionByZeroException)
- {
-     if(b==0)
-        throw DivisionByZeroException();
-    return a/b;
- }
+double division(double a, double b)
+{
+    if (b == 0)
+        throw DivisionByZeroException("Dividing by zero is not allowed");
+    return a / b;
+}
 
- int main() {
+int main()
+{
     double result;
-    try {
-        result = division(13,0);
-        cout<<"The result is = "<<result<<endl;
-    }
-    //This catch block handles DivisionByZeroException type of exception only
-    catch(DivisionByZeroException e) {
-        e.getErrorMessage();
-    }
-    //This catch block handles all other types of exceptions
-    catch(...)
+    try
     {
-        cout<<"Error !"<<endl;
+        result = division(13, 0);
+        cout << "The result is = " << result << endl;
+    }
+    // This catch block handles DivisionByZeroException type of exception only
+    catch (DivisionByZeroException &e)
+    {
+        cout << e.what() << endl;
+    }
+    // This catch block handles all other types of exceptions
+    catch (...)
+    {
+        cout << "Error !" << endl;
     }
     return 0;
 }
